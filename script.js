61ecba626e4ae8675d93395d878635b871ab8d26
SileneG <silenegonzalezquiroz@gmail.com>
2024-08-05 21:44:42 -0500

    Se hizo modificaciones en el scrip.js y style.css

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const textarea = document.getElementById("texto");
-    const initialPlaceholder = "Digite el texto a encriptar o desencriptar";
-
-    textarea.addEventListener("focus", function() {
-        textarea.placeholder = initialPlaceholder;
-    });
-});
-
 function fnencriptar() {
     const texto = document.getElementById("texto").value.trim();
     const mensajeEncrip = document.getElementById("mensaje-encrip");
@@ -16,24 +7,22 @@ function fnencriptar() {
     const copiarTextoBtn = document.querySelector(".copiar-texto");
     const textarea = document.getElementById("texto");
 
-    // Validar entrada de texto
     if (texto === "") {
         textarea.placeholder = "Debe ingresar un texto para encriptar";
         return;
     }
 
     if (!/^[a-z ]+$/.test(texto)) {
-        textarea.value = ""; // Borrar el texto
+        textarea.value = ""; 
         textarea.placeholder = "Solo se aceptan letras minúsculas sin acentos ni caracteres especiales.";
         return;
     }
 
-    // Ocultar imagen
     muneco.style.display = "none";
     mensajeEncrip.style.display = "flex";
-    mensajeEncrip.style.alignItems = "center";
+    mensajeEncrip.style.alignItems = "flex-start"; 
+    document.querySelector('.text-encriptado').style.justifyContent = 'flex-start';
 
-    // Mostrar texto encriptado
     let textoEncriptado = texto.replace(/e/g, "enter")
                                .replace(/i/g, "imes")
                                .replace(/a/g, "ai")
@@ -42,7 +31,6 @@ function fnencriptar() {
     tituloMensaje.textContent = "Texto Encriptado con éxito";
     textoInicial.textContent = textoEncriptado;
 
-    // Mostrar botón de copiar
     copiarTextoBtn.style.display = "block";
 }
 
@@ -55,24 +43,22 @@ function fndesencriptar() {
     const copiarTextoBtn = document.querySelector(".copiar-texto");
     const textarea = document.getElementById("texto");
 
-    // Validar entrada de texto
     if (texto === "") {
         textarea.placeholder = "Debe ingresar un texto para desencriptar";
         return;
     }
 
     if (!/^[a-z ]+$/.test(texto)) {
-        textarea.value = ""; // Borrar el texto
+        textarea.value = ""; 
         textarea.placeholder = "Solo se aceptan letras minúsculas sin acentos ni caracteres especiales.";
         return;
     }
 
-    // Ocultar imagen
     muneco.style.display = "none";
     mensajeEncrip.style.display = "flex";
-    mensajeEncrip.style.alignItems = "center";
+    mensajeEncrip.style.alignItems = "flex-start";
+    document.querySelector('.text-encriptado').style.justifyContent = 'flex-start';
 
-    // Mostrar texto desencriptado
     let textoDesencriptado = texto.replace(/enter/g, "e")
                                   .replace(/imes/g, "i")
                                   .replace(/ai/g, "a")
@@ -81,13 +67,29 @@ function fndesencriptar() {
     tituloMensaje.textContent = "Texto Desencriptado";
     textoInicial.textContent = textoDesencriptado;
 
-    // Mostrar botón de copiar
     copiarTextoBtn.style.display = "block";
 }
 
 function fncopiar() {
     const textoInicial = document.getElementById("texto-inicial").textContent;
-    navigator.clipboard.writeText(textoInicial).then(() => {
-        alert("Texto copiado al portapapeles");
-    });
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(textoInicial).then(() => {
+            alert("Texto copiado");
+        }).catch(err => {
+            console.error('Error al copiar el texto: ', err);
+        });
+    } else {
+        const textArea = document.createElement("textarea");
+        textArea.value = textoInicial;
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        try {
+            document.execCommand('copy');
+            alert('Texto copiado');
+        } catch (err) {
+            console.error('Error al copiar el texto: ', err);
+        }
+        document.body.removeChild(textArea);
+    }
 }
